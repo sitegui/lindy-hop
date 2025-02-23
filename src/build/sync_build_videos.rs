760289107570feb2761b98a675672aae7da58c9d
sitegui <sitegui@sitegui.com.br>
2025-02-23 20:52:43 +0100

    Overwrite on build re-encoded videos

--- src/build/sync_build_videos.rs
+++ src/build/sync_build_videos.rs
@@ -9,8 +9,15 @@ pub fn sync_build_videos(source_dir: &Path, destination_dir: &Path) -> anyhow::R
 
     for source in videos {
         let destination = destination_dir.join(source.strip_prefix(source_dir)?);
+        let source_size = fs::metadata(&source)?.len();
         if !fs::exists(&destination)? {
             fs::copy(source, destination)?;
+        } else {
+            let destination_size = fs::metadata(&destination)?.len();
+            if source_size != destination_size {
+                log::info!("Will overwrite {}", destination.display());
+                fs::copy(source, destination)?;
+            }
         }
     }
 
